463def9bd1529dcd5abed91628211859ad9dbfbb
Mohed <mccdama@gmail.com>
2021-10-25 22:31:04 +0200

    First draft - TCP

--- 1_Networking_and_Concurrency/2_Guess_Game/lux2/app/src/main/java/lux2/app/TCPClient.java
+++ 1_Networking_and_Concurrency/2_Guess_Game/lux2/app/src/main/java/lux2/app/TCPClient.java
@@ -1,47 +1,54 @@
 package lux2.app;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TCPClient {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws UnknownHostException, IOException {
         // arguments supply message and hostname of destination
-        Socket s = null;
-        boolean correct=true;
-        try {
-            int serverPort = 8080;
-            int number;
-            Scanner sc = new Scanner(System.in);
-            s = new Socket(args[1], serverPort);
-            // System.out.println(s);
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            out.writeUTF(args[0]); // UTF is a string encoding
-            String data = in.readUTF();
-            System.out.println("Received: " + data);
-            while (correct) {
-                System.out.println("Enter a number between 0-9");
-                	correct = false;
+        int number, sonuc;
+        boolean correct = true;
+        Scanner sc = new Scanner(System.in);
+        Socket s = new Socket("127.0.0.1", 8080);
+        Scanner sc1 = new Scanner(s.getInputStream());
+        while (correct) {
+
+            System.out.println("Enter a number between 1-10");
+            number = sc.nextInt();
+
+            PrintWriter pout = new PrintWriter(s.getOutputStream(), true);
+            pout.println(number);
+
+            InputStream in = s.getInputStream();
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+            sonuc = Integer.parseInt(bin.readLine());
+            if (sonuc == 1) {
+                System.out.println("You win!");
+                correct = false;
+            } else if (sonuc == 2) {
+                System.out.println("Increase!");
+
+            } else if (sonuc == 3) {
+                System.out.println("Decrease!");
             }
-        } catch (UnknownHostException e) {
-            System.out.println("Socket:" + e.getMessage());
-        } catch (EOFException e) {
-            System.out.println("EOF:" + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO:::" + e.getMessage());
-        } finally {
-            if (s != null)
-                try {
-                    s.close();
-                } catch (IOException e) {
-                    System.out.println("close:" + e.getMessage());
-                }
+
+            if (sonuc == 4) {
+                System.out.println("You Lost!");
+                correct = false;
+            }
         }
+        s.close();
+
     }
 }
