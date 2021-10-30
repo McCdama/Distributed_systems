a43a7b116d3af613021fb7a636aae000a83829cc
Mohed <mccdama@gmail.com>
2021-10-30 13:07:34 +0200

    Semi-worked TCP

--- 1_Networking_and_Concurrency/2_Guess_Game/jsrv/app/src/main/java/jsrv/app/TCPServer.java
+++ 1_Networking_and_Concurrency/2_Guess_Game/jsrv/app/src/main/java/jsrv/app/TCPServer.java
@@ -1,25 +1,15 @@
 package jsrv.app;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Random;
-import java.util.Scanner;
 
 public class TCPServer {
 
-    private static final String SERVER_IP = "127.0.0.1";
+    // private static final String SERVER_IP = "127.0.0.1";
     public static final int PORT = 9090;
 
     private static final int SERVER_READY = 0;
@@ -27,45 +17,45 @@ public class TCPServer {
     private static final int INCORRECT_GUESS = 2;
     private static final int GAME_OVER = 9;
 
+    static Random random;
+    static int attemp = 0;
+    static int randomNumber;
+
     public static void main(String args[]) throws IOException {
         ServerSocket listener = new ServerSocket(PORT);
         System.out.println("[Server] Waiting for connection");
         Socket client = listener.accept();
         System.out.println("[Server] Connected to the Client");
-        System.out.println("[Server] SERVER_READY");
 
         OutputStream out = client.getOutputStream();
         InputStream in = client.getInputStream();
-
+        out.write(SERVER_READY);
+        random = new Random();
+        randomNumber = random.nextInt(9) + 1;
+        System.out.println("Generated Random Number: " + randomNumber);
         try {
             while (true) {
-                Random random = new Random();
-                int randomNumber = random.nextInt(9);
-                int attemp = 0;
-
-                while (attemp != 3) {
-                    attemp++;
-                    out.write(SERVER_READY);
-
+                GAME:
+                while (attemp < 3) {
                     int request = in.read();
-
+                    System.out.println("[Client sends]: " + request);
                     if (randomNumber != request) {
-                        switch (request) {
-                        case INCORRECT_GUESS:
-                            out.write(INCORRECT_GUESS);
-                            break;
-                        case CORRECT_GUESS:
-                        out.write(CORRECT_GUESS);
+                        out.write(INCORRECT_GUESS);
+                        attemp++;
+                        if (attemp==3){
+                            break GAME;
                         }
-                    } else {
+                    } else if (randomNumber == request) {
                         out.write(CORRECT_GUESS);
+                        break GAME;
+                    } else{
+                        out.write(GAME_OVER);
+                        break GAME;
                     }
-                } 
+                }
             }
-        } finally {
-            out.close();
-            in.close();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage());
         }
     }
-
 }
--- 1_Networking_and_Concurrency/2_Guess_Game/lux2/app/src/main/java/lux2/app/TCPClient.java
+++ 1_Networking_and_Concurrency/2_Guess_Game/lux2/app/src/main/java/lux2/app/TCPClient.java
@@ -2,6 +2,7 @@ package lux2.app;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
@@ -24,6 +25,8 @@ public class TCPClient {
     private static final int CORRECT_GUESS = 1;
     private static final int INCORRECT_GUESS = 2;
     private static final int GAME_OVER = 9;
+    private static int attemp = 0;
+
 
     public static void main(String args[]) throws UnknownHostException, IOException {
         Socket socket = new Socket(SERVER_IP, SERVER_PORT);
@@ -33,26 +36,42 @@ public class TCPClient {
 
         OutputStream out = new DataOutputStream(socket.getOutputStream());
 
-        GAME: // outer
+
         while (true) {
             System.out.println("> ");
 
             int command = Integer.parseInt(keyboard.readLine());
-            out.write(command);
-            int serverResponse = input.read();
-            System.out.println("Server Respond " + serverResponse);
+            if (command == 112) break;
+            
+  
+                out.write(command);
+                int serverResponse = input.read();
+                System.out.println("ServerResponse: " + serverResponse);
 
-            switch (serverResponse) {
-             case CORRECT_GUESS:
-                System.out.println("You've won");
-                break GAME;
-             case INCORRECT_GUESS:
-                System.out.println("Try again");
                 
-
-
+                GAME:
+                switch (serverResponse) {
+                case SERVER_READY:
+                    System.out.println("SERVER READY");
+                    break;
+                case CORRECT_GUESS:
+                    System.out.println("You've won");
+                    break GAME;
+                case INCORRECT_GUESS:
+                if (attemp <= 3){
+                    System.out.println("Try again");
+                    attemp++;
+                    break;
+                }
+                case GAME_OVER:
+                if (attemp == 3){
+                    System.out.println("GAME_OVER");
+                    break GAME;
+                }
             }
         }
+        System.out.println("Finished Game...");
+        socket.close();
 
     }
 }
