ec6a14f6d0503e2ab2e02b35f041a98fdb6cda20
Mohed <mccdama@gmail.com>
2021-11-10 00:47:20 +0100

    running the app through CLI

--- 1_Networking_and_Concurrency/3_Web_Crawler/app/src/main/java/_Web_Crawler/WebCrawler.java
+++ 1_Networking_and_Concurrency/3_Web_Crawler/app/src/main/java/_Web_Crawler/WebCrawler.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class WebCrawler {
-    public static void main(String[] cmdArg) {
-
-        String initialURL = cmdArg[0];
+    
+    public static void main(String[] args) {
+        String initialURL = args[0];
         Set<String> emails = new HashSet<>();
 
-        Thread sThread = new CrawlerThread(emails, initialURL);
-        sThread.setDaemon(true);
-        sThread.start();
+        //Thread sThread = new CrawlerThread(emails, initialURL);
+        //sThread.setDaemon(true);
+        //sThread.start();
 
         System.out.println("Press <Enter> to stop crawling..");
-        Scanner keyboard = new Scanner(System.in);
-        keyboard.nextLine();
+        //Scanner keyboard = new Scanner(System.in);
+        //keyboard.nextLine();
 
-        System.out.println("Emails: " + emails);
-        keyboard.close();
+        //System.out.println("Emails: " + emails);
+        //keyboard.close();
     }
 }
