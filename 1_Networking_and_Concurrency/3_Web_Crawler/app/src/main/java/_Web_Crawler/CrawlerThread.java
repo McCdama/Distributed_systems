b0a7095e697c57597bbee13713d8ed36f89ff265
Mohed <mccdama@gmail.com>
2021-11-10 00:31:22 +0100

    user input from console-keyboard

--- 1_Networking_and_Concurrency/3_Web_Crawler/app/src/main/java/_Web_Crawler/CrawlerThread.java
+++ 1_Networking_and_Concurrency/3_Web_Crawler/app/src/main/java/_Web_Crawler/CrawlerThread.java
@@ -1,5 +1,39 @@
 package _Web_Crawler;
 
-public class CrawlerThread {
-    
+import java.io.IOException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class CrawlerThread extends Thread {
+    private Set<String> emails;
+    private Queue<String> links = new LinkedList<>();
+    private List<String> visited = new LinkedList<>();
+    private String initialURL;
+
+    public CrawlerThread(Set<String> emails, String initialURL){
+        this.emails =  emails;
+        this.initialURL =  initialURL;
+        links.add(initialURL);
+    }
+
+    @Override
+    public void run(){
+        while (!links.isEmpty()) {
+            String nextURL = links.remove().trim();
+            visited.add(nextURL);
+            try {
+                
+                System.out.println("Reading form URL"+nextURL);
+                URL myUrl = new URL(nextURL);
+                
+            } catch (IOException e) {
+                System.err.println("Can not read the URL: "+nextURL);
+            }
+        }
+    }
+
+
 }
