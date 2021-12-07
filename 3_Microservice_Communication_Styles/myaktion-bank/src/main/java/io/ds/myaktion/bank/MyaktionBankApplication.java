0aa384ba79fae82dba783b7081b5c5cc3fccdf43
Mohed <mccdama@gmail.com>
2021-12-07 22:16:13 +0100

    Redis Message Listener Container

---
+++ 3_Microservice_Communication_Styles/myaktion-bank/src/main/java/io/ds/myaktion/bank/MyaktionBankApplication.java
@@ -0,0 +1,27 @@
+package io.ds.myaktion.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@SpringBootApplication
+public class MyaktionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaktionBankApplication.class, args);
+	}
+
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("processTransaction"));
+		return container;
+	}
+
+}
--- 3_Microservice_Communication_Styles/myaktion-monitor/README.md
+++ 3_Microservice_Communication_Styles/myaktion-monitor/README.md
@@ -2,4 +2,5 @@ Start the myaktion-monitor Server first
 Connect Browser with SSE-Endpoint (http://localhost:8081/monitor.html)
 and then the myaktion server
 
-Add further new donations to a campaign using REST API of myaktion
+--> Add further new donations to a campaign using REST API of myaktion
+Watch your browser! 
