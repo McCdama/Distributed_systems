5daf485ad98089def51e0c6d6526f64ec5f80481
Mohed <mccdama@gmail.com>
2021-12-07 10:33:02 +0100

    Data Transfer Object

---
+++ 3_Microservice_Communication_Styles/src/main/java/io/ds/myaktion/monitor/dto/ReducedDonation.java
@@ -0,0 +1,38 @@
+package io.ds.myaktion.monitor.dto;
+
+public class ReducedDonation { // This class is needed in both services!
+
+    private String donorName;
+    private Double amount;
+    private Long campaignId;
+
+    public String getDonorName() {
+        return donorName;
+    }
+
+    public void setDonorName(String donorName) {
+        this.donorName = donorName;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(Long campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    @Override
+    public String toString() {
+        return "ReducedDonation [amount=" + amount + ", campaignId=" + campaignId + ", donorName=" + donorName + "]";
+    }
+
+}
