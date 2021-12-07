4272075c39d29acdf442dd9d417cf8780c659c76
Mohed <mccdama@gmail.com>
2021-12-07 10:15:51 +0100

    Add myaktion-monitor folder

---
+++ 3_Microservice_Communication_Styles/src/main/java/io/ds/myaktion/monitor/MyaktionMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.ds.myaktion.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaktionMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaktionMonitorApplication.class, args);
+	}
+
+}
